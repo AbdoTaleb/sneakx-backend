adc026dff5ae66bf93a33848aceebb2850b7c086
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2026-03-17 03:42:16 +0100

    adding frontend to program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "https://sneakx-frontend.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
